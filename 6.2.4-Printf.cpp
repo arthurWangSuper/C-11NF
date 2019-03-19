00d98002639210a0f2bcc8c4f9d1ade7de8ce437
ArthurWangSuper <cp3wt10@163.com>
2019-03-19 19:51:19 +0800

    Create 6.2.4-Printf.cpp

---
+++ 6.2.4-Printf.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+void Printf(const char* s){
+	while(*s){
+		if(*s == '%' && *++s != '%')
+			throw runtime_error("invalid format string: missing arguments");
+		cout << *s++;
+	}
+}
+
+template<typename T,typename... Args>
+void Printf(const char *s,T value, Args... args){
+	while(*s){
+		if(*s == '%' && *++s != '%'){
+			cout << value;
+			return Printf(++s,args...);
+		}
+		cout << *s++;
+	}
+	throw runtime_error("extra arguments provided to Printf");
+}
+
+int main()
+{
+	Printf("hello %s %s\n",string("world"),string("ars"));
+}
